a345957363d044822e17662e27951517aebf1bed
Damian van Zeelt <71325072+Vortex-Blade@users.noreply.github.com>
2024-12-09 16:41:08 +0100

    Create Automation.py

---
+++ Automation.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+foreach{
+  pyauotogui.click(1067, 498)
+  time.sleep(1)
+  pyautogui.typewrite(3)
+  time.sleep(1)
+  pyauotogui.click(1067, 498)
+  time.sleep(1)
+  pyautogui.typewrite(1)
+  time.sleep(1)
+  }

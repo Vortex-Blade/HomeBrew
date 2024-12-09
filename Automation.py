93234bb6b688147c02ca3ccbd86f6d577d8f9d8d
Damian van Zeelt <71325072+Vortex-Blade@users.noreply.github.com>
2024-12-09 17:18:53 +0100

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -1,12 +1,12 @@
 import pyautogui
 import time
-foreach{
+while True:
   pyauotogui.click(1067, 498)
-  time.sleep(1)
+  time.sleep(1000)
   pyautogui.typewrite(3)
-  time.sleep(1)
+  time.sleep(1000)
   pyauotogui.click(1067, 498)
-  time.sleep(1)
+  time.sleep(1000)
   pyautogui.typewrite(1)
-  time.sleep(1)
-  }
+  time.sleep(1000)
+

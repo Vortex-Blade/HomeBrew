33f4973af6b274c5f047d4c9f44b9f0a0055b4d2
Damian van Zeelt <71325072+Vortex-Blade@users.noreply.github.com>
2024-12-10 12:22:21 +0100

    Update Automation.py

--- Automation.py
+++ Automation.py
@@ -1,12 +1,12 @@
 import pyautogui
 import time
-while True:
-  pyauotogui.click(1067, 498)
-  time.sleep(1000)
-  pyautogui.typewrite(3)
-  time.sleep(1000)
-  pyauotogui.click(1067, 498)
-  time.sleep(1000)
-  pyautogui.typewrite(1)
-  time.sleep(1000)
 
+while True:
+    pyautogui.click(1067, 498)
+    time.sleep(1.5)
+    pyautogui.press('3')
+    time.sleep(1.5)
+    pyautogui.click(1067, 498)
+    time.sleep(1.5)
+    pyautogui.press('1')
+    time.sleep(1.5)
